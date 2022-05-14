e51926455f32c0f43b4629a1b04853fb00a7d507
Shoaib Akhtar <sk370008@gmail.com>
2022-05-14 16:57:57 +0530

    signUp_screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import { useContext } from 'react';
 import SigninScreen from './screens/SigninScreen.js';
 import { Store } from './Store.js';
 import ShippingAddressScreen from './screens/ShippingAddressScreen.js';
+import SignupScreen from './screens/SignupScreen.js';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -76,6 +77,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
               <Route
                 path="/shipping"
                 element={<ShippingAddressScreen />}
